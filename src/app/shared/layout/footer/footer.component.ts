610f9c0a8156fecb3b6acff4a3bdb02c6d04a1c7
alceanicu <alceanicu@users.noreply.github.com>
2019-08-05 17:22:51 +0300

    Update footer.component.ts

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-layout-footer',
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   public today: number = Date.now();
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
