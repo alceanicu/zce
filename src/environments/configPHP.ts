cde3fc9c2dd6974b4bc812db1ac539c6d3d7f2b8
NICOLAE ALCEA <nicolae.alcea@rcs-rds.local>
2019-12-04 17:21:02 +0200

    v 2.1.0

--- src/environments/configPHP.ts
+++ src/environments/configPHP.ts
@@ -2,9 +2,5 @@ export const configPHP = {
   phpPath: 'phpQuestions',
   configPath: 'appConfig',
   max: 806,
-  correctOptions: [
-    {value: true, text: 'YES'},
-    {value: false, text: 'NO'}
-  ],
   apiUrl: 'http://localhost/rest-api/'
 };
