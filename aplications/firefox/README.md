e760c373ed0d3b1c805c64e662fc58a630dccdbf
NICOLAE ALCEA <nicolae.alcea@rcs-rds.local>
2019-12-06 16:32:03 +0200

    v 2.1.0

--- aplications/firefox/README.md
+++ aplications/firefox/README.md
@@ -1,8 +1,7 @@
 ### FIREFOX extension
 
 ```
-coment `useHash: true` in app-routing.module.ts
-ng build --prod --base-href="./" --aot --buildOptimizer=true --extractLicenses=true --optimization=true
+ng build --prod --aot --buildOptimizer=true --extractLicenses=true --optimization=true 
 COPY manifest.js &&  background.js TO dist/zce
 ```
 
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from '@app/app.module';
 import { environment } from '@env/environment';
+// import { TAB_ID } from '@app/tab-id.injector';
 
 if (environment.production) {
   enableProdMode();
@@ -12,3 +13,13 @@ platformBrowserDynamic()
   .bootstrapModule(AppModule)
   // .then(success => console.log(`Bootstrap success`))
   .catch(error => console.error(error));
+
+// chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+//   const tab = [...tabs].pop();
+//   const {id: tabId} = tab;
+//
+//   // provides the current Tab ID so you can send messages to the content page
+//   platformBrowserDynamic([{provide: TAB_ID, useValue: tabId}])
+//     .bootstrapModule(AppModule)
+//     .catch(error => console.error(error));
+// });
