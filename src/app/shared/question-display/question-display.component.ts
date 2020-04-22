adf0f6425ae33266a877f631d64805f856d66583
nicu alcea <nicu.alcea@gmail.com>
2020-04-22 16:56:16 +0300

    v 3.0.0

--- src/app/shared/question-display/question-display.component.ts
+++ src/app/shared/question-display/question-display.component.ts
@@ -1,9 +1,7 @@
 import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 
-import { Logger, Question } from '@app/core';
-import { PhpHighlightingLanguage, PhpQuestionType } from '@app/core/enum/config';
-
-const log = new Logger('QuestionDisplayComponent');
+import { Question } from '@app/core';
+import { PhpHighlightingLanguage } from '@app/core/enum/config';
 
 @Component({
   selector: 'app-question-display',
@@ -14,7 +12,6 @@ const log = new Logger('QuestionDisplayComponent');
 export class QuestionDisplayComponent implements OnInit, AfterViewInit {
   @Input() public question: Question;
   public PhpHighlightingLanguage = PhpHighlightingLanguage;
-  public PhpQuestionType = PhpQuestionType;
 
   constructor(
     private cdr: ChangeDetectorRef
@@ -22,12 +19,9 @@ export class QuestionDisplayComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
-    // this.userAnswer = 0;
-    // log.info('on ngOnInit');
   }
 
   ngAfterViewInit(): void {
-    // log.info('on ngAfterViewInit');
     this.cdr.detectChanges();
   }
 }
