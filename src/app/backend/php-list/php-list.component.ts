47f2f7287c98473472e0234479d5db7e6c6d8e13
nicu alcea <nicu.alcea@gmail.com>
2020-04-24 14:32:13 +0300

    v 3.0.0

--- src/app/backend/php-list/php-list.component.ts
+++ src/app/backend/php-list/php-list.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
@@ -10,8 +11,8 @@ import { switchMap, takeUntil } from 'rxjs/operators';
 import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
 
 import { environment } from '@env/environment';
-import { IQuestion } from '@app/core';
-import { PhpQuestionDifficulty, PhpQuestionCategory, PhpQuestionType } from '@app/core/enum/config';
+import { IAnswerRow, IQuestion, IQuestionRow } from '@app/core';
+import { PhpHighlightingLanguage, PhpQuestionCategory, PhpQuestionDifficulty, PhpQuestionType } from '@app/core/enum/config';
 
 @Component({
   selector: 'app-php-list',
@@ -36,6 +37,7 @@ export class PhpListComponent implements OnInit, AfterViewInit, OnDestroy {
   length: number;
   pageSize: number;
   pageSizeOptions: number[];
+  fileUrl: any;
 
   public questionList$: Observable<IQuestion[]>;
   public page$: BehaviorSubject<number>;
@@ -44,6 +46,7 @@ export class PhpListComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(
     private readonly db: AngularFirestore,
     private router: Router,
+    private sanitizer: DomSanitizer
   ) {
     this.length = environment.configPHP.max;
     this.pageSize = 5;
@@ -86,15 +89,64 @@ export class PhpListComponent implements OnInit, AfterViewInit, OnDestroy {
     this.unsubscribe$.complete();
   }
 
-  public onPageClick($event: PageEvent) {
-    console.log($event);
+  public onPageClick($event: PageEvent): void {
     this.pageSize = $event.pageSize;
     this.page$.next($event.pageIndex + 1);
   }
 
-  public editQuiz(id: number) {
+  public editQuiz(id: number): void {
     this.router
       .navigate(['/backend/php-edit/' + id])
       .then(() => console.log(id));
+  }
+
+  public getFileName(id: number): string {
+    return `${id}`.padStart(4, '0') + '.md';
+  }
+
+  public generateMdFile(question: IQuestion): void {
+    const blobParts = [];
+
+    blobParts.push(`[<<< Previous question <<<](${this.getFileName(question.id - 1)})`);
+    blobParts.push(`   Question ID#${this.getFileName(question.id)}   `);
+    blobParts.push(`[>>> Next question >>>](${question.id + 1}.md)\r\n\r\n`);
+
+    question.questionRows.forEach((value: IQuestionRow, index, array) => {
+      if (value.language !== PhpHighlightingLanguage.NONE) {
+        blobParts.push('```' + value.language + '\r\n');
+      }
+      blobParts.push(`${value.text}\r\n`);
+      if (value.language !== PhpHighlightingLanguage.NONE) {
+        blobParts.push('```' + '\r\n');
+      }
+    });
+
+    blobParts.push('\r\n');
+
+    const abcd = ['A', 'B', 'C', 'D'];
+    let correct = '';
+
+    question.answerRows.forEach((value: IAnswerRow, index) => {
+      if (value.language === PhpHighlightingLanguage.NONE) {
+        blobParts.push(`- [ ] ${abcd[index]}) ${question.answerRows[index].text}`);
+      } else {
+        blobParts.push(`- [ ] ${abcd[index]})`);
+        blobParts.push(`${question.answerRows[index].text}`);
+      }
+      blobParts.push(`\r\n`);
+      if (value.isCorrect) {
+        correct += `${abcd[index]}, `;
+      }
+    });
+    correct = correct.slice(0, -2);
+
+    blobParts.push(`\r\n<details><summary><b>Answer</b></summary>\r\n`);
+    blobParts.push(`<p>\r\n`);
+    blobParts.push(`  Answer: <strong>${correct}</strong>\r\n`);
+    blobParts.push(`</p>\r\n`);
+    blobParts.push(`</details>\r\n`);
+
+    const blob = new Blob(blobParts, { type: 'text/markdown' });
+    this.fileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(window.URL.createObjectURL(blob));
   }
 }
