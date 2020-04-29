45a86d4f8b346b553e5914758010f1cdec50680d
nicu alcea <nicu.alcea@gmail.com>
2020-04-29 12:42:32 +0300

    v 3.0.0

--- src/environments/configPHP.ts
+++ src/environments/configPHP.ts
@@ -4,5 +4,8 @@ export const configPHP = {
   max: 807,
   examSize: 70,
   examTime: 5400,
-  doNotRepeatInlasts: 10
+  doNotRepeatInlasts: 10,
+  // generatedBy: { name: 'ZCE Chrome Extension', link: 'https://chrome.google.com/webstore/detail/zce/kdjolhghoglghipajmbmlmldbpncimge' },
+  // generatedBy: { name: 'ZCE Firefox Extension', url: 'https://addons.mozilla.org/ro/firefox/addon/php-zend-certification-exam/' },
+  generatedBy: { name: 'ZCE GitHub Page', url: 'https://alceanicu.github.io/zce/#/home' }
 };
